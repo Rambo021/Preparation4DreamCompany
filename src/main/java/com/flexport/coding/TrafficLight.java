739e5e7a21bcc32ad1679bbce565ced96d311482
panyong <panyong@ztocc.com>
2024-03-27 22:22:17 +0800

    3

--- src/main/java/com/flexport/coding/TrafficLight.java
+++ src/main/java/com/flexport/coding/TrafficLight.java
@@ -1,6 +1,11 @@
 package com.flexport.coding;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @description 红绿灯
@@ -24,30 +29,54 @@ import java.util.List;
  */
 public class TrafficLight {
 
+    public static void main(String[] args) {
+
+        Map city = init();
+
+        Car car = new Car("car", 0,0, 2);
+        System.out.printf("min:" + city.simulate(car));
+    }
+
+    public static Map init(){
+        Road A = new Road("A", 2, 2, true);
+        Road B = new Road("B", 2, 1, true);
+        Road J = new Road("J", 1, 3, false);
+
+        Light AJ = new Light(1,2);
+        Light BJ = new Light(1,1);
+
+        Map city = new Map(Arrays.asList(A,B,J), Arrays.asList(AJ, BJ), 2, 3);
+
+        return city;
+    }
 
     /**
      * 道路：水平或垂直
      * 水平：(null, y)
      * 垂直：(x, null)
      */
-    class Road{
+    static class Road{
+        private String name;
         Integer x;
         Integer y;
+        Boolean horizontal;
 
-        public Road(Integer x, Integer y) {
+        public Road(String name, Integer x, Integer y, Boolean horizontal) {
+            this.name = name;
             this.x = x;
             this.y = y;
+            this.horizontal = horizontal;
         }
 
-        public boolean Horizontal(){
-            return x == null;
+        public boolean horizontal(){
+            return horizontal;
         }
     }
 
     /**
      * 路灯：坐标 + 颜色
      */
-    class Light{
+    static class Light{
         Integer x, y;
         boolean green;
 
@@ -67,10 +96,11 @@ public class TrafficLight {
      * 城市地图：定义城市边界和内部道路与十字路口的路灯
      * 默认城市坐标轴原点为(0,0)
      */
-    class Map{
+    static class Map{
         List<Road> roads;
         List<Light> lights;
 
+        // 城市地图的边界
         Integer x;
         Integer y;
 
@@ -88,25 +118,53 @@ public class TrafficLight {
          * @return
          */
         public Integer simulate(Car car){
+            java.util.Map<String, Light> lightMap = lights.stream().collect(Collectors.toMap(light -> String.format("(%s,%s)",light.x, light.y), Function.identity()));
 
-            return 0;
+            int min = 0;
+            // 当车辆的坐标超出边界时结束
+            for (; x >= car.x && y >= car.y && car.x >= 0 && car.y >= 0; min++){
+                // 先判断当前是否在路口以及路口红绿灯颜色；如果没有红绿灯或者为绿灯，那么车辆直接沿着前进方向向前一步
+                Light light = lightMap.get(String.format("(%s,%s)",car.x, car.y));
+                if (light == null || light.green){
+                    // 行驶方向：1：从西到东；2：从东到西；3：从北到南；4：从南到北
+                    switch (car.direction){
+                        case 1:
+                            car.x += 1;
+                            break;
+                        case 2:
+                            car.x -= 1;
+                            break;
+                        case 3:
+                            car.y += 1;
+                            break;
+                        case 4:
+                            car.y -= 1;
+                            break;
+                    }
+                }
+
+                // 然后切换红绿灯颜色
+                lights.forEach(l -> l.toggle());
+            }
+            return min;
         }
     }
 
     /**
      * 车辆：名称 + 起点坐标
      */
-    class Car{
+    static class Car{
         String name;
         Integer x;
         Integer y;
         // 行驶方向：1：从西到东；2：从东到西；3：从北到南；4：从南到北
         Integer direction;
 
-        public Car(String name, Integer x, Integer y) {
+        public Car(String name, Integer x, Integer y, Integer direction) {
             this.name = name;
             this.x = x;
             this.y = y;
+            this.direction = direction;
         }
     }
 }
