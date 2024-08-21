20b789a5b9cdd4d856cd8b30894833594941b630
rambo <panyong@ztocc.com>
2024-08-21 20:54:30 +0800

    新增题目

--- src/main/java/com/booking/coding/Backspacing.java
+++ src/main/java/com/booking/coding/Backspacing.java
@@ -11,4 +11,26 @@ package com.booking.coding;
  * @date 2024/8/21 14:55:15
  */
 public class Backspacing {
+
+    public static void main(String[] args) {
+        String str = "abc##def#";
+        System.out.printf(backspacing(str));
+    }
+
+    public static String backspacing(String str){
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == '#'){
+                if (sb.length() != 0){
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+            }else {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
 }
