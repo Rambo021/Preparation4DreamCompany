b3bf546b7c02ed026134fbf9aecc90903fdc2758
rambo <panyong@ztocc.com>
2024-08-21 14:58:13 +0800

    Backspacing

---
+++ src/main/java/com/booking/coding/Backspacing.java
@@ -0,0 +1,14 @@
+package com.booking.coding;
+
+/**
+ * @author ooo
+ * @description 实现空格删除字符
+ * i/p -> "abc#def##"
+ * o/p -> "abd"
+ *
+ * 可以使用StringBuffer简单实现
+ * 输入字符非 # 则 append；否则删除最后一个字符 deleteAt()
+ * @date 2024/8/21 14:55:15
+ */
+public class Backspacing {
+}
